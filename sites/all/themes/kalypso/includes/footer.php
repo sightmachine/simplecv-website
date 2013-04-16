f45d12d2a5bde2d9da4c316e2f1496e1edf6381d
matthewaho <ahomatthew@gmail.com>
2013-04-16 14:23:50 -0400

    Footer stuff

--- sites/all/themes/kalypso/includes/footer.php
+++ sites/all/themes/kalypso/includes/footer.php
@@ -7,11 +7,36 @@ function kalypso_footer($page){
     <div id="footer"> 
       <div class="container">
           <div class="content">
-            <a href="href="http://sightmachine.com"><img src="<?php print file_create_url(drupal_get_path('theme', 'kalypso')); ?>/images/smlogo.png" /></a> <span style="margin-left: 20px;">Copyright <a href="http://sightmachine.com">Sight Machine</a>, Inc, SimpleCV is a trademark of Sight Machine, Inc.</span>
+            <div class="links">
+              <ul>
+                <li>
+                  <a href="https://plus.google.com/115199279159361214026/posts" target="_blank">Google+</a>
+                </li>
+                <li>
+                  <a href="http://www.facebook.com/pages/SimpleCV/150136128401869" target="_blank">Facebook</a>
+                </li>
+                <li>
+                  <a href="http://twitter.com/Simple_CV" target="_blank">Twitter</a>
+                </li>
+                <li>
+                  <a href="https://github.com/ingenuitas/simplecv" target="_blank">Github</a>
+                </li>
+                <li>
+                  <a href="http://help.simplecv.org/" target="_blank">Help</a>
+                </li>
+              </ul>
+            </div>
+            <div class="copyright">
+              Copyright <a href="http://sightmachine.com">Sight Machine</a>, Inc, SimpleCV is a trademark of Sight Machine, Inc.
+            </div>
+            <div class="footer-logo">
+              <a href="href="http://sightmachine.com"><img src="<?php print file_create_url(drupal_get_path('theme', 'kalypso')); ?>/images/smlogo.png"/></a>
+            </div>
           </div>
         
       </div> 
     </div>
     <!-- end footer --> 
-<?php }
+<?php 
+  }
 ?>
